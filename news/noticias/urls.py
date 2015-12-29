fe9b720a63e09ce904bf8ff46008c17dbb755df5
Jose alejandro Soriano garcia <josealejandrosorianogarcia@MacBook-Pro-de-Jose.local>
2015-12-29 17:12:43 -0600

    actualizacion Noticias list-edit

--- news/noticias/urls.py
+++ news/noticias/urls.py
@@ -5,15 +5,18 @@ from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
-	url(r'^eventos$', views.evento_nueva, name='evento_nueva'),
-	url(r'^noticias$', views.noticia_nueva, name='noticia_nueva'),
+	url(r'^eventos/$', views.evento_nueva, name='evento_nueva'),
+	url(r'^noticias/$', views.noticia_nueva, name='noticia_nueva'),
 
-	url(r'^eventos/list$', views.evento_list),
-	url(r'^eventos/(?P<pk>[0-9]+)/$', views.post_detail),
+	url(r'^eventos/list$', views.evento_list, name='evento_list'),
+	url(r'^eventos/(?P<pk>[0-9]+)/$', views.evento_detail, name='evento_detail'),
+
+	url(r'^noticias/list$', views.noticia_list, name='noticia_list'),
+	url(r'^noticias/(?P<pk>[0-9]+)/$', views.noticia_detail, name='noticia_detail'),
 
 
 	url(r'^eventos/(?P<pk>[0-9]+)/edit/$', views.evento_edit, name='evento_edit'),
-	#url(r'^noticias/(?P<pk>[0-9]+)/edit/$', views.noticia_edit, name='noticia_edit'),
+	url(r'^noticias/(?P<pk>[0-9]+)/edit/$', views.noticia_edit, name='noticia_edit'),
 
 
 ]
--- news/noticias/views.py
+++ news/noticias/views.py
@@ -7,13 +7,16 @@ def evento_list(request):
 	posts = Eventos.objects.filter(fecha__lte=timezone.now()).order_by('fecha')
         return render(request, 'noticias/evento_list.html', {'posts': posts})
 
+def noticia_list(request):
+	posts = Noticias.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+        return render(request, 'noticias/noticia_list.html', {'posts': posts})
 
 def evento_nueva(request):
         if request.method == "POST":
 		form = EventoForm(request.POST)
 		if form.is_valid():
-			form.save()
-			return render(request, 'noticias/base2.html',{})
+			post = form.save()
+			return redirect('noticias.views.evento_detail', pk=post.pk)
 	else:
 		form = EventoForm()
 	return render(request, 'noticias/post_edit.html', {'form': form, 'tipo' : 'Evento'})
@@ -22,26 +25,38 @@ def noticia_nueva(request):
 	if request.method == "POST":
 		form = NoticiaForm(request.POST)
 		if form.is_valid():
-			form.save()
-			return render(request, 'noticias/base2.html',{})
+			post = form.save()
+			return redirect('noticias.views.noticia_detail', pk=post.pk)
 	else:
 		form = NoticiaForm()
 	return render(request, 'noticias/post_edit.html', {'form': form, 'tipo' : 'Noticia'})
 
-def post_detail(request, pk):
+def evento_detail(request, pk):
         post = get_object_or_404(Eventos, pk=pk)
         return render(request, 'noticias/evento_detail.html', {'post': post, 'tipo' : 'Evento'})
-        
+
+def noticia_detail(request, pk):
+        post = get_object_or_404(Noticias, pk=pk)
+        return render(request, 'noticias/noticia_detail.html', {'post': post, 'tipo' : 'Noticia'})
 
 def evento_edit(request,pk):
 		post = get_object_or_404(Eventos, pk=pk)
 		if request.method == "POST":
 			form = EventoForm(request.POST, instance=post)
 			if form.is_valid():
-				form.save
-				return redirect('noticias.views.post_detail', pk=post.pk)
+				form.save()
+				return redirect('noticias.views.evento_detail', pk=post.pk)
 		else:
 			form = EventoForm(instance=post)
 		return render(request, 'noticias/post_edit.html', {'form': form, 'tipo' : 'Evento', 'post' : post})
 
-#def noticia_edit(request):
+def noticia_edit(request,pk):
+		post = get_object_or_404(Noticias, pk=pk)
+		if request.method == "POST":
+			form = NoticiaForm(request.POST, instance=post)
+			if form.is_valid():
+				form.save()
+				return redirect('noticias.views.noticia_detail', pk=post.pk)
+		else:
+			form = NoticiaForm(instance=post)
+		return render(request, 'noticias/post_edit.html', {'form': form, 'tipo' : 'Noticia', 'post' : post})
